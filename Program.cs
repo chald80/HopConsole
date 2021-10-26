09cdb85686bafa0c540e40e06849751560a3218a
Anders Hald <chald80@gmail.com>
2021-10-26 19:55:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using HopClassLib;
+
+
+namespace HopConsole
+{
+    class Program
+    {
+     
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            List<Hop> Hops = new List<Hop>
+            {
+                new Hop {Id = 1, Name = "1", HarvestYear = 1, AlphaAcid = 1, Price = 1},
+                new Hop {Id = 2, Name = "2", HarvestYear = 2, AlphaAcid = 2, Price = 2}
+
+            };
+            foreach (Hop h in Hops)
+            {
+                Console.WriteLine($"ID :{h.Id} Name : {h.Name}, HarvestYear : {h.HarvestYear}, AlphaAcid : {h.AlphaAcid} Price : {h.Price}");
+                
+            }
+
+
+
+
+
+        }
+    }
+}
